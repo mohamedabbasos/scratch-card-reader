5cf761a032d3074a07749391a361816f1501b868
Mohamed Abbas <m.abbas.sudan@gmail.com>
2017-07-09 13:46:45 +0300

    Some config updates + HELLO Message for test button click

--- app/src/main/java/com/example/dpteam/cardreader/MainActivity.java
+++ app/src/main/java/com/example/dpteam/cardreader/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.dpteam.cardreader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.print("Hello People!");
+        //Toast.makeText(MainActivity.this,"Test toast",Toast.LENGTH_LONG).show();
+
+        Button btn =(Button)findViewById(R.id.start);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Hello DP Team",Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+
 }
 
